2756d4daf4aefe73e0a2256874f365f7bcc2f91e
AmansGit <amans1997a@gmail.com>
2024-02-29 11:04:25 +0530

    Company class added

--- EMS.py
+++ EMS.py
@@ -21,36 +21,102 @@ class Employee:
 
 
 class Department:
-	def __init__(self, dept_name, emp_list: list):
+	def __init__(self, dept_name):
 		self.dept_name = dept_name
-		self.employees = []
+		self.employees = {}
 
-	def add_employee(self, emp_id, title, department):
-		self.employee_details.append(
-			)
-		{
+	def add_employee(self, emp_name, emp_id, title, department):
+		self.employees[emp_id] = {
+			"name": emp_name,
 			"emp_id": emp_id,
 			"title": title,
 			"department": department
-		}
-
+			}
+		print(f"Inside Add employee: {self.employees}")
+# {
+# 	1: {"emp_id": 1, "name": "Aman", "Dept": "engg"},
+# 	2: {"emp_id": 2, "name": "Akash", "Dept": "Analytics"}
+# }
 
 	def rm_employee(self, emp_id):
-		pass
+		if emp_id in employees:
+			del employees[emp_id]
+
+		# If we initialize employee as dict and inserting data in List then:
+		for employee in self.employees:
+			if employee.emp_id == emp_id:
+				self.employees.remove(employee)
+				print(f"Employee with ID {employee_id} removed from {self.name}")
+				return
+		print(f"Employee with emp-id: {emp_id} not available")
+
 
 	def list_employees(self):
-		pass
+		if not self.employees:
+			print("No employees in this department.")
+		else:
+			print(f"Employees in {self.dept_name}")
+			for employee in employees:
+				print(employee)
+
+class Company:
+	def __init__(self):
+		self.departments = {}
+
+	def add_department(self, department):
+		""" To add new department
+		"""
+		self.departments[department.dept_name] = department
+
+	def rm_department(self, department_name):
+		""" To remove any department based on name
+		"""
+		if department_name in self.departments:
+			del self.departments[department_name]
+			print(f"Department {department_name} successfully removed")
+
+		else:
+			print(f"Department {self.department_name} is not available")
+
+	def show_departments(self):
+		if not self.departments:
+			print("No departments to show now")
+
+		else:
+			print("Departments: ")
+			for depts in self.departments:
+				print(f"- {depts}")
 
 
 
+# Working with company directory
+company = Company()
+
+ece_dept = Department("ECE")
+cse_dept = Department("CSE")
+
+company.add_department(ece_dept)
+company.add_department(cse_dept)
+
+
+company.show_departments()
+
+company.rm_department("ECE")
+
+# Displaying departments after removal
+company.show_departments()
 
 
 
-emp1 = Employee("Aman Sharma", 1, "Software Engineer", "Engineering")
-emp2 = Employee("Akash Sharma", 2, "Data Scientist", "Analytics")
+emp1 = Employee("Aman Sharma", 1, "SDE", "ECE")
+emp2 = Employee("Akash Sharma", 2, "Sr SDE", "CSE")
 Employee.show_emp_details(emp2)
 
+
+# print("OBJECT:: ", emp1)
 # eng_dept = Department("Engineering")
+ece_dept.add_employee(emp1)
+print("AFTER ECE_DEPT")
 # analytic_dept = Department("Analytics")
 
 
